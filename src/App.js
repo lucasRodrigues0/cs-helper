c29b646ce23ce8612d86f9f8388b9f0750ac68d6
Lucas Rodrigues <lucasrodrigues119816@gmail.com>
2024-01-18 16:43:57 -0300

    implementação inicial do filtro

--- src/App.js
+++ src/App.js
@@ -1,14 +1,43 @@
 import './App.css';
 import { Galeria } from './components/galeria';
 import { videosMock } from './mocks/videos';
-import { Filtro} from './components/filtro'
+import { Filtro} from './components/filtro';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [galeria, setGaleria] = useState(videosMock);
+  const [galeriaOriginal, setGaleriaOriginal] = useState(videosMock);
+
+  const onClickSelectMapa = (event) => {
+
+    const mapa = event.target.value;
+
+    if (mapa === "" || mapa === "Todos") {
+
+      setGaleria(galeriaOriginal);
+
+    } else {
+
+      const filtrado = galeriaOriginal.videos.filter((video) => video.mapa === mapa); 
+
+      setGaleria({
+        ...galeria,
+        videos: filtrado
+      })
+    }   
+
+  }
+
+  useEffect(() => {
+    setGaleriaOriginal(videosMock);
+  }, []);
+
   return (
     <div className='container'>
       <div className="App">
-        <Filtro />
-        <Galeria galeria={videosMock} />      
+        <Filtro onClickSelectMapa={onClickSelectMapa}/>
+        <Galeria galeria={galeria} />      
       </div>
     </div>
   );
--- src/components/filtro/Filtro.js
+++ src/components/filtro/Filtro.js
@@ -1,12 +1,17 @@
-export const Filtro = () => {
+import './style.css'
+
+export const Filtro = ({onClickSelectMapa}) => {
   return (
-    <div>
-      <select>
-        <option>Selecione o mapa</option>
+    <div className="filtro">
+      <select onChange={onClickSelectMapa}>
+        <option>Todos</option>
         <option>Mirage</option>
-        <option>Inferno</option>
         <option>Overpass</option>
         <option>Vertigo</option>
+        <option>Inferno</option>
+        <option>Ancient</option>
+        <option>Anubis</option>
+        <option>Nuke</option>
       </select>
     </div>
   )
